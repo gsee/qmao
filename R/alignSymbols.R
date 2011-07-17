3dbaad55a8615c152438c1f63e4a1795cac70e44
gsee <gsee@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-07-17 23:29:18 +0000

    - make sure data are xts or zoo
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/twsinstrument/pkg/qmao@84 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/alignSymbols.R
+++ R/alignSymbols.R
@@ -5,17 +5,22 @@ function(symbols, env=.GlobalEnv) {
 	if (any(!is.character(symbols))) 
 		stop("Symbols must be vector of character strings.")
 	ff <- try(get(symbols[1],pos=env))
-	if (inherits(ff, 'try-error')) stop('You must create an xts object for each symbol first.')
+	if (inherits(ff, 'try-error') || (!inherits(ff,'xts') && !inherits(ff,'zoo'))) stop('You must create an xts object for each symbol first.')
 	ncols <- ncol(ff)	#compare all symbols ncols to 1st one; make sure they're the same
-	for (sym in symbols[-1]) {
-		tmp.sym <- get(sym,pos=env)
-		if (ncol(tmp.sym) != ncols) stop("All symbols must have the same number of columns.")
-		ff <- merge(ff,tmp.sym,all=FALSE)
-		ff <- na.omit(ff)
+    symout <- symbols[1]	
+    for (sym in symbols[-1]) {
+		tmp.sym <- try(get(sym,pos=env),silent=TRUE)
+        if (!inherits(tmp.sym,'try-error') && (inherits(tmp.sym,'xts') || inherits(tmp.sym,'zoo'))) {
+		    if (ncol(tmp.sym) != ncols) stop("All symbols must have the same number of columns.")
+		    ff <- merge(ff,tmp.sym,all=FALSE)
+		    ff <- na.omit(ff)
+            symout <- c(symout, sym)
+        }
 	}
-	for (sym in symbols) {
+	for (sym in symout) {
         asym <- strsplit(sym,"_")[[1]][1] #only use root of futures or options in col names
 		assign(sym,ff[,grep(asym,colnames(ff))],pos=env)
 	}
-	symbols
+	symout
 }
+
