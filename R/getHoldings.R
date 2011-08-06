749093bc75bdb0090372b0c0bbe03d5018d358e8
gsee <gsee@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-08-06 17:03:34 +0000

    add auto.assign arg to getHoldings.
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/twsinstrument/pkg/qmao@111 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/getHoldings.R
+++ R/getHoldings.R
@@ -3,15 +3,11 @@
 #There is a link called "Most Recent Net Asset Values" on https://www.spdrs.com/product/
 #it has all the SPDR funds, and it should be used here instead of just the sectorspdrs.
 
-getHoldings <-function(symbols, env=.GlobalEnv) {
-	tmp <- tempfile()
-	download.file("http://us.ishares.com/product_info/fund/excel_profile.htm",destfile=tmp)
-	ishr.syms <- read.csv(tmp)
-	ishr.syms <- as.character(ishr.syms$Fund.Name)[-1]
-	unlink(tmp)
+getHoldings <-function(symbols, env=.GlobalEnv, auto.assign=TRUE) {
 	spdr.syms <- c('XLY','XLP','XLE','XLF','XLV','XLI','XLB','XLK','XLU')
 	if (missing(symbols)) symbols <- c(ishr.syms,spdr.syms)
 	ishr.out <- spdr.out <- NULL
+    if (length(symbols) > 1 && !auto.assign) stop('auto.assign must be TRUE for more than 1 symbol.')
 	for (symbol in symbols) {
 		if(!is.na(match(symbol,spdr.syms))) {
 			tmp <- tempfile()
@@ -23,22 +19,31 @@ getHoldings <-function(symbols, env=.GlobalEnv) {
 			colnames(fr) <- paste(symbol,'Weight',sep='.')
 			#fr2 <- data.frame(as.character(fr[,3]),as.character(fr[,2]),as.numeric(fr[,4]))
 			#colnames(fr) <- c('Symbol','Name','Weight')
-			assign(paste(symbol,'h',sep='.'),fr,pos=env)
+			if (auto.assign) assign(paste(symbol,'h',sep='.'),fr,pos=env)
 			spdr.out <- c(spdr.out,paste(symbol,'h',sep='.'))
-		} else if(!is.na(match(symbol,ishr.syms))) {		
-			tmp <- tempfile()
-			download.file(paste('http://us.ishares.com/product_info/fund/excel_holdings.htm?ticker=',symbol,sep=""), destfile=tmp)
-			fr <- read.csv(tmp,skip=11)
-			unlink(tmp)
-			fr <- fr[1:(length(fr[,1])-3),c(1,3)]
-			fr <- fr[fr$Symbol!='--',] #maybe this is dangerous, but I'm ignoring stuff with Symbol=="--" (e.g. BLACKROCK FDS III)
-			#colnames(fr) <- c('Symbol','Name',paste(symbol,'Weight',sep='.'))
-			fr <- data.frame(fr[,2],row.names=fr[,1])			
-			colnames(fr) <- paste(symbol,'Weight',sep='.')
-			assign(paste(symbol,'h',sep='.'),fr,pos=env)
-			ishr.out <- c(ishr.out,paste(symbol,'h',sep='.'))
-		} else stop("Unrecognized ETF. Make sure your symbols are either iShares or Sector SPDRs.")	
+		} else {
+            tmp <- tempfile()
+	        download.file("http://us.ishares.com/product_info/fund/excel_profile.htm",destfile=tmp)
+	        ishr.syms <- read.csv(tmp)
+	        ishr.syms <- as.character(ishr.syms$Fund.Name)[-1]
+	        unlink(tmp)
+            if(!is.na(match(symbol,ishr.syms))) {		
+			    tmp <- tempfile()
+			    download.file(paste('http://us.ishares.com/product_info/fund/excel_holdings.htm?ticker=',symbol,sep=""), destfile=tmp)
+			    fr <- read.csv(tmp,skip=11)
+			    unlink(tmp)
+			    fr <- fr[1:(length(fr[,1])-3),c(1,3)]
+			    fr <- fr[fr$Symbol!='--',] #maybe this is dangerous, but I'm ignoring stuff with Symbol=="--" (e.g. BLACKROCK FDS III)
+			    #colnames(fr) <- c('Symbol','Name',paste(symbol,'Weight',sep='.'))
+			    fr <- data.frame(fr[,2],row.names=fr[,1])			
+			    colnames(fr) <- paste(symbol,'Weight',sep='.')
+                if (auto.assign) assign(paste(symbol,'h',sep='.'),fr,pos=env)
+			    ishr.out <- c(ishr.out,paste(symbol,'h',sep='.'))
+            } else stop("Unrecognized ETF. Make sure your symbols are either iShares or Sector SPDRs.")
+		} 	
 	}
-	paste(c(ishr.out,spdr.out))
+    if (auto.assign) {
+    	paste(c(ishr.out,spdr.out))
+    } else fr
 }
 
