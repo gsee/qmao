34f050da3ae1bdcfaa9098b9c872cab42fd56f1e
gsee <gsee@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-11-20 16:58:19 +0000

    do.rbind convenience fun (forgot to svn add in last commit)
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/twsinstrument/pkg/qmao@204 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/do.rbind.R
@@ -0,0 +1,42 @@
+#' Split into \code{f} [days], apply a function to each
+#' \code{f} [day], and rbind the results together
+#'
+#' this is a simple convenience function for this type of
+#' usage:
+#' \code{do.call(rbind, lapply(split(x, f)), FUN)}
+#'
+#' From the \code{\link[xts]{endpoints}} help page:
+#' Valid values for the argument \code{f} include: \dQuote{us} (microseconds),
+#' \dQuote{microseconds}, \dQuote{ms} (milliseconds),
+#' \dQuote{milliseconds}, \dQuote{secs} (seconds),
+#' \dQuote{seconds}, \dQuote{mins} (minutes), \dQuote{minutes},
+#' \dQuote{hours}, \dQuote{days}, \dQuote{weeks}, \dQuote{months}, \dQuote{quarters},
+#' and \dQuote{years}.
+#'
+#' @param x xts object
+#' @param FUN function to apply to each period
+#' @param f string describing the period to split by
+#' @param indexAt Currently only "lastof" (Default) and "firstof" are supported.  
+#' The index will be the last (first) time of each period.
+#' @return xts
+#' @author Garrett See
+#' @examples
+#' data(sample_matrix)
+#' x <- as.xts(sample_matrix)
+#' do.rbind(x, NROW, 'weeks')
+#' do.rbind(x, last, 'weeks')
+#' @export
+do.rbind <- function(x, FUN, f='days', indexAt='lastof') {
+    x <- try.xts(x)
+    FUN <- match.fun(FUN)
+    switch(indexAt, 
+        lastof={
+            idx <- do.call(rbind, lapply(split(x,f), last))
+            if (identical(FUN, last)) return(idx)
+        },
+        firstof={
+            idx <- do.call(rbind, lapply(split(x,f), first))
+            if (identical(FUN, first)) return(idx)
+        })    
+    xts(do.call(rbind, lapply(split(x, f), FUN)), index(idx))
+}
