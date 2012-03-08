c1e41ed76618124f15132252775324e8c25e7bff
gsee <gsee@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-03-08 16:01:57 +0000

    - alignSymbols can handle data that does not have a Symbol in the colnames  - replace "\t" with "    "  - change symbols formal to Symbols
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/twsinstrument/pkg/qmao@249 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/alignSymbols.R
+++ R/alignSymbols.R
@@ -1,51 +1,65 @@
-#' Delete rows that not all symbols have in common
+#' Delete rows that not all Symbols have in common
 #' 
-#' Deletes rows that not all symbols have in common
+#' Deletes rows that not all Symbols have in common
 #' 
-#' alignSymbols will get symbols from the specified environment (.GlobalEnv by
+#' alignSymbols will get Symbols from the specified environment (.GlobalEnv by
 #' default), merge them together, and delete all rows that have any NAs.  Then
 #' it will \sQuote{unmerge} and assign the cleaned data back to the xts objects
-#' named by \code{symbols}.
+#' named by \code{Symbols}.
 #' 
-#' @param symbols character vector of symbol names which correspond to xts
+#' @param Symbols character vector of symbol names which correspond to xts
 #' objects
 #' @param env The environment that holds the xts objects
 #' @return used for its side effect.
 #' @author Garrett See
-#' @seealso \code{\link{gsa}}, \code{\link{PF}}, merge, merge.xts, cbind, cbind.xts, assign
+#' @seealso \code{\link{gsa}}, \code{\link{PF}}, merge, merge.xts, cbind, 
+#' cbind.xts, assign
 #' @examples
 #' 
 #' \dontrun{
-#' symbols <- c("SPY","DIA","CORN")
-#' getSymbols(symbols)
+#' Symbols <- c("SPY","DIA","CORN")
+#' getSymbols(Symbols)
 #' length(Cl(SPY));length(Cl(DIA));length(Cl(CORN))
-#' alignSymbols(symbols)
+#' alignSymbols(Symbols)
 #' length(Cl(SPY));length(Cl(DIA));length(Cl(CORN))
 #' }
 #' @export
 alignSymbols <-
-function(symbols, env=.GlobalEnv) {
-	if (length(symbols) < 2) 
-		stop("Must provide at least 2 symbols")
-	if (any(!is.character(symbols))) 
-		stop("Symbols must be vector of character strings.")
-	ff <- try(get(symbols[1],pos=env))
-	if (inherits(ff, 'try-error') || (!inherits(ff,'xts') && !inherits(ff,'zoo'))) stop('You must create an xts object for each symbol first.')
-	ncols <- ncol(ff)	#compare all symbols ncols to 1st one; make sure they're the same
-    symout <- symbols[1]	
-    for (sym in symbols[-1]) {
-		tmp.sym <- try(get(sym,pos=env),silent=TRUE)
-        if (!inherits(tmp.sym,'try-error') && (inherits(tmp.sym,'xts') || inherits(tmp.sym,'zoo'))) {
-		    if (ncol(tmp.sym) != ncols) stop("All symbols must have the same number of columns.")
-		    ff <- merge(ff,tmp.sym,all=FALSE)
-		    ff <- na.omit(ff)
+function(Symbols, env=.GlobalEnv) {
+    if (length(Symbols) < 2) 
+        stop("Must provide at least 2 Symbols")
+    if (any(!is.character(Symbols))) 
+        stop("Symbols must be vector of character strings.")
+    ff <- try(get(Symbols[1], pos=env))
+    if (inherits(ff, 'try-error') || 
+            (!inherits(ff,'xts') && !inherits(ff,'zoo'))) {
+        stop('You must create an xts object for each symbol first.')
+    }
+    # add the symbol and an underscore to the column names of each xts so that
+    # we can "unmerge" easier
+    colnames(ff) <- paste(Symbols[1], colnames(ff), sep="_") 
+    ncols <- ncol(ff) # we'll make sure all Symbols ncols is same as 1st one
+    symout <- Symbols[1]
+    for (sym in Symbols[-1]) {
+        tmp.sym <- try(get(sym,pos=env),silent=TRUE)
+        if (!inherits(tmp.sym,'try-error') && 
+                (inherits(tmp.sym,'xts') || inherits(tmp.sym,'zoo'))) {
+            if (ncol(tmp.sym) != ncols) {
+                stop("All Symbols must have the same number of columns.")
+            }
+            colnames(tmp.sym) <- paste(sym, colnames(tmp.sym), sep="_")
+            ff <- merge(ff,tmp.sym,all=FALSE)
+            ff <- na.omit(ff)
             symout <- c(symout, sym)
         }
-	}
-	for (sym in symout) {
-        asym <- strsplit(sym,"_")[[1]][1] #only use root of futures or options in col names
-		assign(sym,ff[,grep(asym,colnames(ff))],pos=env)
-	}
-	symout
+    }
+    for (sym in symout) {
+        tmpid <- paste(sym, "_", sep="")
+        cols <- grep(tmpid, colnames(ff))
+        tmpout <- ff[, cols]
+        colnames(tmpout) <- sub(tmpid, "", colnames(tmpout))
+        assign(sym, tmpout, pos=env)
+    }
+    symout
 }
 
