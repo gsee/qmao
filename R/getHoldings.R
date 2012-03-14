85493b1e2c296254d4c306e375dfa3c2c766b91d
gsee <gsee@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-03-14 00:35:26 +0000

    updates to getHoldings:  - symbols formal renamed to Symbols  - return class is c("weights", "data.frame")  - if more than one holding has the same Symbol, a "duplicates" attribute is    added and rownames are made unique.
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/twsinstrument/pkg/qmao@250 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/getHoldings.R
+++ R/getHoldings.R
@@ -11,17 +11,22 @@
 #' 
 #' Only iShares and the 8 Sector SPDR ETFs are supported.
 #' 
-#' @param symbols chr symbols of ETFs
+#' Some iShares ETFs have more than one holding listed with the same Symbol.
+#' In the case that Symbols are duplicated, a \dQuote{duplicates} attribute
+#' will be added to the returned object.  Also, since rownames cannot be 
+#' duplicated, they will be made unique with \code{\link{make.unique}}
+#' 
+#' @param Symbols chr Symbols of ETFs
 #' @param env environment to store the holdings data in
 #' @param auto.assign assign data?
-#' @return If called with \code{auto.assign=TRUE}, this function is called for
-#' side-effect. Holdings data are stored in variables that are \code{symbols}
-#' appended with .h Right now, all that is stored is the symbols and their
-#' weights. If called with \code{auto.assign=FALSE}, Holdings data will be
-#' returned, and not stored.
+#' @return An object classed as \dQuote{weights} will be created that is a 
+#' data.frame with columns for holdings' weights and names.  If called with 
+#' \code{auto.assign=TRUE}, it will be assigned in \code{env} with names that 
+#' are \code{Symbols} appended with \dQuote{.h}.  Otherwise, the 
+#' \dQuote{weights} will be returned and not stored.
 #' @note This should get SPDR data from a csv that can be downloaded at
 #' https://www.spdrs.com/product. Could also be updated to return more info
-#' than just weights and symbols.
+#' than just weights and Symbols.
 #' @author Garrett See
 #' @references Sector SPDRs \url{www.sectorspdr.com} iShares
 #' \url{www.ishares.com}
@@ -34,20 +39,22 @@
 #' IVE.h
 #' }
 #' @export
-getHoldings <-function(symbols, env=.GlobalEnv, auto.assign=TRUE) {
+getHoldings <-function(Symbols, env=.GlobalEnv, auto.assign=TRUE) {
 	spdr.syms <- c('XLY','XLP','XLE','XLF','XLV','XLI','XLB','XLK','XLU')
-	if (missing(symbols)) symbols <- c(ishr.syms,spdr.syms)
+	if (missing(Symbols)) Symbols <- c(ishr.syms,spdr.syms)
 	ishr.out <- spdr.out <- NULL
-    if (length(symbols) > 1 && !auto.assign) stop('auto.assign must be TRUE for more than 1 symbol.')
-	for (symbol in symbols) {
+    if (length(Symbols) > 1 && !auto.assign) stop('auto.assign must be TRUE for more than 1 symbol.')
+	for (symbol in Symbols) {
 		if(!is.na(match(symbol,spdr.syms))) {
 			tmp <- tempfile()
 			download.file(paste("http://www.sectorspdr.com/content/?do=indexComposition&symbol=", 
 								symbol, "&filetype=csv", sep=""), destfil=tmp) 
 			fr <- read.csv(tmp,sep="\t",stringsAsFactors=FALSE)
 			unlink(tmp)
-			fr <- data.frame(as.numeric(fr[,4]),row.names=as.character(fr[,3]))			
-			colnames(fr) <- paste(symbol,'Weight',sep='.')
+			fr <- data.frame(fr[, c(4, 2)], row.names=as.character(fr[,3]),
+                             stringsAsFactors=FALSE)			
+			colnames(fr) <- c(paste(symbol,'Weight',sep='.'), "Name")
+            class(fr) <- c("weights", "data.frame")
 			#fr2 <- data.frame(as.character(fr[,3]),as.character(fr[,2]),as.numeric(fr[,4]))
 			#colnames(fr) <- c('Symbol','Name','Weight')
 			if (auto.assign) assign(paste(symbol,'h',sep='.'),fr,pos=env)
@@ -61,16 +68,25 @@ getHoldings <-function(symbols, env=.GlobalEnv, auto.assign=TRUE) {
             if(!is.na(match(symbol,ishr.syms))) {		
 			    tmp <- tempfile()
 			    download.file(paste('http://us.ishares.com/product_info/fund/excel_holdings.htm?ticker=',symbol,sep=""), destfile=tmp)
-			    fr <- read.csv(tmp,skip=11)
+			    fr <- read.csv(tmp, skip=11, stringsAsFactors=FALSE)
 			    unlink(tmp)
-			    fr <- fr[1:(length(fr[,1])-3),c(1,3)]
+			    fr <- fr[1:(length(fr[,1])-3),c(1:3)]
 			    fr <- fr[fr$Symbol!='--',] #maybe this is dangerous, but I'm ignoring stuff with Symbol=="--" (e.g. BLACKROCK FDS III)
 			    #colnames(fr) <- c('Symbol','Name',paste(symbol,'Weight',sep='.'))
-			    fr <- data.frame(fr[,2],row.names=gsub(" ", "", fr[,1]))			
-			    colnames(fr) <- paste(symbol,'Weight',sep='.')
+			    fr[, 1] <- gsub(" ", "", fr[, 1])
+                rowsyms <- fr[, 1]
+                dupes <- character(0)
+                if (any(duplicated(rowsyms))) {
+                    dupes <- rowsyms[duplicated(rowsyms)]
+                    warning(paste("Some Symbols duplicated:", paste(dupes, collapse=" ")))
+                }
+                fr <- data.frame(fr[, 3:2], row.names=make.unique(rowsyms))
+			    colnames(fr) <- c(paste(symbol,'Weight',sep='.'), "Name")
+                if (length(dupes) > 0) attr(fr, "duplicates") <- dupes
+                class(fr) <- c("weights", "data.frame")
                 if (auto.assign) assign(paste(symbol,'h',sep='.'),fr,pos=env)
 			    ishr.out <- c(ishr.out,paste(symbol,'h',sep='.'))
-            } else stop("Unrecognized ETF. Make sure your symbols are either iShares or Sector SPDRs.")
+            } else stop("Unrecognized ETF. Make sure your Symbols are either iShares or Sector SPDRs.")
 		} 	
 	}
     if (auto.assign) {
