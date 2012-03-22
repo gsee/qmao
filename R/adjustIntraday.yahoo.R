791a4947b495ee1b1e52f2225934bf610b418d5f
gsee <gsee@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-03-22 23:54:19 +0000

    || where there should have been &&
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/twsinstrument/pkg/qmao@256 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/adjustIntraday.yahoo.R
+++ R/adjustIntraday.yahoo.R
@@ -111,7 +111,7 @@ get_div <- function(Symbol, force=FALSE, silent=TRUE) {
     if (is.null(instr$div) || force) {
         div <- try(getDividends(Symbol))
         if (!inherits(div, 'try-error')) {
-            if (length(div) > 1 || !is.na(div)) colnames(div) <- paste(Symbol, "div", sep=".")            
+            if (length(div) > 1 && !is.na(div)) colnames(div) <- paste(Symbol, "div", sep=".")            
             instr$div <- div
             assign(Symbol, instr, pos=FinancialInstrument:::.instrument)
         } else return(NULL)
@@ -135,7 +135,7 @@ get_spl <- function(Symbol, force=FALSE, silent=TRUE) {
     if (is.null(instr$spl) || force) {
         spl <- getSplits(Symbol)
         if (!inherits(spl, 'try-error')) {
-            if (length(spl) > 1 || !is.na(spl)) colnames(spl) <- paste(Symbol, "spl", sep=".")            
+            if (length(spl) > 1 && !is.na(spl)) colnames(spl) <- paste(Symbol, "spl", sep=".")            
             instr$spl <- spl
             assign(Symbol, instr, pos=FinancialInstrument:::.instrument)
         } else (return(NULL))
