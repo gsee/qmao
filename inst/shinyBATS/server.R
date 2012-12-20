15849032f0f3aa221abafa018af3bf9e73cbf713
gsee <gsee@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-12-20 14:13:39 +0000

    use qmao::getQuote.BATS in shiny app so that the app can be launched with qmao::shinyBATS() even if qmao is not attached
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/twsinstrument/pkg/qmao@357 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- inst/shinyBATS/server.R
+++ inst/shinyBATS/server.R
@@ -1,6 +1,6 @@
 shinyServer(function(input, output) {
   output$plot_symbol <- reactivePlot(function() { 
-    plot(getQuote(toupper(input$Symbol), src='BATS', what=input$what, 
+    plot(qmao::getQuote.BATS(toupper(input$Symbol), what=input$what, 
          exch=input$exch))
     invalidateLater(1000)
   })
