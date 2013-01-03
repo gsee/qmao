8d40876b06d536fec698095f2f2a413a3d0b7b0f
gsee <gsee@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-01-03 18:33:33 +0000

    translate tolower outside of match.arg call
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/twsinstrument/pkg/qmao@388 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/getQuote.BATS.R
+++ R/getQuote.BATS.R
@@ -82,11 +82,13 @@
 #' @export
 #' @rdname getQuote.BATS
 getQuote.BATS <- function(Symbols, 
-                          what=c("bats", "bbo", "ladder", "depth", "trades"), #, "most.active"), 
+                          what=c("bbo", "bats", "ladder", "depth", "trades"), #, "most.active"), 
                           exch=c("bzx", "byx", "opt")) {
   #require(RJSONIO) # added at top of script; fromJSON will be Imported from RJSONIO in NAMESPACE
-  what <- match.arg(tolower(what))
-  exch <- match.arg(tolower(exch))
+  what <- tolower(what)
+  exch <- tolower(exch)
+  what <- match.arg(what)
+  exch <- match.arg(exch)
   
   # if Symbols is more than 1 symbol, then return a multi-row data.frame if 
   # what="bbo" or a list if what is anything else
