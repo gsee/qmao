71999672d60050020a684214812560c15a77adfd
gsee <gsee@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-04-30 15:10:01 +0000

    Require a later version of XML.  The last bug was due to an XML change, so if a user had a previous version of XML, the new version of getEarnings would not work. Some tests added, but they would not have helped in this situation.
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/twsinstrument/pkg/qmao@412 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ inst/tests/test-getEarnings.R
@@ -0,0 +1,17 @@
+context("makePriceFrame")
+
+test_that("return.class of xts", {
+  earn <- getEarnings("AAPL")
+  expect_identical(colnames(earn), c("EPS.ESTIMATE", "EPS.ACTUAL", "PREV.YEAR.ACTUAL"))
+  expect_match(earn$SYMBOL[1], "AAPL")
+})
+
+test_that("return.class of data.frame", {
+  earn <- getEarnings("AAPL", return.class="data.frame")
+  expect_identical(colnames(earn), 
+                   c("SYMBOL", "PERIOD", "EVENT.TITLE", "EPS.ESTIMATE", 
+                     "EPS.ACTUAL", "PREV.YEAR.ACTUAL", "TIME"))
+  expect_match(earn$SYMBOL[1], "AAPL")
+})
+
+
