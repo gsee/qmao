95a17ca106e0f7df75c1932e9ebc227c7b2d9383
gsee <gsee@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-07-31 13:01:42 +0000

    remove tests for functions that no longer work because Reuters does not want them to work for free.
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/twsinstrument/pkg/qmao@423 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- inst/tests/test-.getDividendCalendar.R
+++ inst/tests/test-.getDividendCalendar.R
@@ -1,12 +1,14 @@
-context(".getDividendCalendar")
+#context(".getDividendCalendar")
 
-test_that("file downloads correctly", {
-  dc <- .getDividendsCalendar()
-  expect_identical(colnames(dc), 
-        c("SYMBOL", "COMPANY", "AMOUNT", "EX-DATE", "PAYABLE", "RECORD", "DECLARATION"))
-  expect_identical(sapply(dc, class),
-                   structure(c("character", "character", "numeric", "Date", "Date", "Date", "Date"), 
-                             .Names = c("SYMBOL", "COMPANY", "AMOUNT", "EX-DATE", "PAYABLE", "RECORD", "DECLARATION")))
+# .getDividendCalendar doesn't work anymore because Reuters doesn't like you.
 
-})
+#test_that("file downloads correctly", {
+#  dc <- .getDividendsCalendar()
+#  expect_identical(colnames(dc), 
+#        c("SYMBOL", "COMPANY", "AMOUNT", "EX-DATE", "PAYABLE", "RECORD", "DECLARATION"))
+#  expect_identical(sapply(dc, class),
+#                   structure(c("character", "character", "numeric", "Date", "Date", "Date", "Date"), 
+#                            .Names = c("SYMBOL", "COMPANY", "AMOUNT", "EX-DATE", "PAYABLE", "RECORD", "DECLARATION")))
+#
+#})
 
--- inst/tests/test-getEarnings.R
+++ inst/tests/test-getEarnings.R
@@ -1,17 +1,19 @@
-context("getEarnings")
+#context("getEarnings")
 
-test_that("return.class of xts", {
-  earn <- getEarnings("AAPL")
-  expect_identical(colnames(earn), c("EPS.ESTIMATE", "EPS.ACTUAL", "PREV.YEAR.ACTUAL"))
-  expect_match(earn$SYMBOL[1], "AAPL")
-})
+# getEarnings isn't working because Reuters doesn't like you
 
-test_that("return.class of data.frame", {
-  earn <- getEarnings("AAPL", return.class="data.frame")
-  expect_identical(colnames(earn), 
-                   c("SYMBOL", "PERIOD", "EVENT.TITLE", "EPS.ESTIMATE", 
-                     "EPS.ACTUAL", "PREV.YEAR.ACTUAL", "TIME"))
-  expect_match(earn$SYMBOL[1], "AAPL")
-})
+#test_that("return.class of xts", {
+#  earn <- getEarnings("AAPL")
+#  expect_identical(colnames(earn), c("EPS.ESTIMATE", "EPS.ACTUAL", "PREV.YEAR.ACTUAL"))
+#  expect_match(earn$SYMBOL[1], "AAPL")
+#})
+
+#test_that("return.class of data.frame", {
+#  earn <- getEarnings("AAPL", return.class="data.frame")
+#  expect_identical(colnames(earn), 
+#                   c("SYMBOL", "PERIOD", "EVENT.TITLE", "EPS.ESTIMATE", 
+#                     "EPS.ACTUAL", "PREV.YEAR.ACTUAL", "TIME"))
+#  expect_match(earn$SYMBOL[1], "AAPL")
+#})
 
 
